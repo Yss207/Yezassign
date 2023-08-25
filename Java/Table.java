10df950b17546859ab7d76f6cd2250b7fa3b9096
Yss207 <133109961+Yss207@users.noreply.github.com>
2023-08-25 10:59:08 +0530

    file to folder

---
+++ Java/Table.java
@@ -0,0 +1,17 @@
+//Table of 2
+import java.util.*;
+
+public class Table{
+       public static void main (String args[]){
+          
+          Scanner sc = new Scanner(System.in);
+          
+          int i;
+          int n = 2;
+          
+          for(i = 1; i<=10; i++){
+              System.out.println(n*i);
+          }
+          
+    }
+}
