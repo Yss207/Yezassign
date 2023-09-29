ebb948fd0cb59f4eaf9636c3ba45188ad18248fb
Yash Sanjay Suryawanshi <133109961+Yss207@users.noreply.github.com>
2023-09-29 10:21:51 +0000

    Yep

---
+++ Java/Abstract.java
@@ -0,0 +1,48 @@
+//abstract class and method stuff
+abstract class Shape{
+    abstract public void Compute_area(int a, int b);
+
+    abstract public void Calculate_Area();//Declaring the method which is to be overriden ahead in the program
+}
+
+class Triangle extends Shape{
+    public int base;
+    public int height;
+    public void Compute_area(int base, int height){
+        this.base = base;
+        this.height = height;
+    }
+
+    public void Calculate_Area(){
+        System.out.println("Area of triangle is: "+(0.5*(base*height)));
+    }
+}
+
+class Rectangle extends Shape{
+    public int length;
+    public int breadth;
+    public void Compute_area(int length, int breadth){
+        this.length = length;
+        this.breadth = breadth;
+    }
+
+    public void Calculate_Area(){
+        System.out.println("Area of rectangle is: "+(length*breadth));
+    }
+}
+
+class Abstract{
+    public static void main(String[] args) {
+        Shape t1 = new Triangle();
+        t1.Compute_area(5, 5);
+        t1.Calculate_Area();
+
+        Shape t2 = new Rectangle();
+        t2.Compute_area(5, 5);
+        t2.Calculate_Area();
+    }
+}
+
+/*
+ * Same program can be optimised with the help of constructors
+ */
